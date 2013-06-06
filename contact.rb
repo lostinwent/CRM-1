87d82573120ca6c8e55d5f4be20f6321a9ff5437
coolmatt <themattemmons@gmail.com>
2013-06-06 15:09:09 -0400

    working through database class

--- contact.rb
+++ contact.rb
@@ -1,28 +1,33 @@
-# the contact class should have five attributes: "id", "firstname", "lastname", "email" and "notes".
-# when a user creates a contact, all five attributes are mandatory.
-# I am able to modify those attributes at any point in the program at a later time.
-# Contact class should also have a method that displays all of its values.
+# # the contact class should have five attributes: "id", "firstname", "lastname", "email" and "notes".
+# # when a user creates a contact, all five attributes are mandatory.
+# # I am able to modify those attributes at any point in the program at a later time.
+# # Contact class should also have a method that displays all of its values.
 
 class Contact
 
   attr_accessor :id, :firstname, :lastname, :email, :notes
+  # @@id = 1000
 
-  def initialize(args)
-    @id         = args[:id] || default_id
-    @firstname  = args[:firstname]
-    @lastname   = args[:lastname]
-    @email      = args[:email]
-    @notes      = args[:notes]
+
+  # bob = Contact.new(1001, "bob", "romney", "mormondad@gmail.com")
+  # raise ArgumentError
+  def initialize(id, firstname, lastname, email, notes)
+    @id         = id
+    @firstname  = firstname
+    @lastname   = lastname
+    @email      = email
+    @notes      = notes
   end
 
-
-  def default_id
-    1000
+  def contact_display
+   puts "ID: #{@id}"
+   puts "First name: #{@firstname}"
+   puts "Last name: #{@lastname}"
+   puts "Email: #{@email}"
+   puts "Notes: #{@notes}"
   end
 
 end
 
-# bob = Contact.new(firstname: "Bob", id: 420)
-# p bob
-
-
+# joe = Contact.new(15, "joe", "schmoe", "email", "this is joe")
+# joe.contact_display
--- database.rb
+++ database.rb
@@ -3,12 +3,47 @@
   # "add", "modify_contact", "display_all_contacts", "display_particular_contact",
   # "display_info_by_attribute" and "delete_contact".
 
+require_relative "contact"
+
 class Database
 
-  attr_accessor :contacts
+  attr_accessor :contacts, :refid
 
   def initialize
     @contacts = []
   end
 
+  def add(id, firstname, lastname, email, notes)
+  # refid = Contact.new(id, firstname, lastname, email, notes)
+    @contacts << Contact.new(id, firstname, lastname, email, notes)
+  end
+
+  def modify_contact
+
+  end
+
+  def display_all_contacts
+    @contacts.each do |contact|
+      contact.contact_display
+      puts ""
+    end
+  end
+
+  def display_particular_contact(keyword)
+    @contacts.find { |i| i[keyword] }
+  end
+
+  def display_info_by_attribute
+  end
+
+  def delete_contact()
+  end
+
 end
+
+db = Database.new
+bob = db.add(58, "Anish", "K", "ak@ak.com", "notes1")
+db.add(23,"betty","l","betty@betty.com","notes")
+# p db.contacts
+p db.display_particular_contact("Anish")
+# p bob.refid
